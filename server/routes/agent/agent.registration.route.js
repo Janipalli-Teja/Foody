0950dbf2cb0cdde8dbc8a2203464df04a37f657e
Janipalli Teja <tejajanipalli29@gmail.com>
2025-09-09 22:21:18 +0530

    normalized datamodels of restaurant|agent

--- server/routes/agent/agent.registration.route.js
+++ server/routes/agent/agent.registration.route.js
@@ -1,31 +1,34 @@
-const express=require('express');
-const upload=require('../../utils/uploadFile.js');
+const express = require('express');
+const upload = require('../../utils/uploadFile.js');
 
 //importing model
-const Agent_Application=require('../../models/delivery_agent/agent_recruitment_approval.model.js');
+const Agent_Application = require('../../models/delivery_agent/agent_profile.model.js');
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/",upload.single('license_number_img'),async (req,res)=>{
-    const handled_admin_id='662e5c03bbf5bc7e4c123abc';
-    const{fullname,phone_number,email,vehicle_number,license_number,approval_status}=req.body;
-    
-    if(!fullname || !phone_number || !email||!vehicle_number|| !license_number|| !approval_status){
-        res.status(400).json({"msg":"fields are missing"});
+router.post("/", upload.single('license_number_img'), async (req, res) => {
+    const { full_name, phone_number, email, vehicle_number, license_number, approval_status } = req.body;
+
+    if (!full_name || !phone_number || !email || !vehicle_number || !license_number || !approval_status) {
+        return res.status(400).json({ "msg": "fields are missing" });
     }
-    if(!req.file){
-        res.status(400).json({"msg":"image not uploaded"});
+    if (!restaurant_license_number) {
+        return res.status(400).json({ msg: "License number is required" });
     }
-    const license_img_url=req.file.path;
 
-    try{
-        const newApplication= new Agent_Application({fullname,phone_number,email,vehicle_number,license_img_url,license_number,handled_admin_id,approval_status});
+    if (!req.file) {
+        return res.status(400).json({ "msg": "image not uploaded" });
+    }
+    const license_img_url = req.file.path;
+
+    try {
+        const newApplication = new Agent_Application({ full_name, phone_number, email, vehicle_number, license_img_url, license_number, approval_status });
         await newApplication.save();
-        res.status(201).json({"msg":"application submitted",data:newApplication});
-    }catch(err){
+        return res.status(201).json({ "msg": "application submitted", data: newApplication });
+    } catch (err) {
         console.log(err);
-        res.status(500).json({"msg":"Server error","error":err.message})
+        return res.status(500).json({ "msg": "Server error", "error": err.message })
     }
 });
 
-module.exports=router;
+module.exports = router;
